8cf8d0a0b731c223b2cc0573cb1ca1cb762f686a
digRabbit <digrabbit@protonmail.com>
2020-09-27 17:28:36 +0300

    go fix

--- installer.sh
+++ installer.sh
@@ -14,7 +14,7 @@ apt install python3-pip -y
 
 #Installing goLang
 wget -O /tmp/go.tar.gz https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/profile
 source /etc/profile
 
